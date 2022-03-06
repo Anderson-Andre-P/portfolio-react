79e47b56caf1b9b09f65fd0955f86f79311fda7f
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-03-06 13:58:10 -0300

    feat: hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Seja bem vindo a <br />
+        Portfólio pessoal
+      </SectionTitle>
+      <SectionText>
+        Sou um desenvolvedor front-end apaixonado por dar vida ao design.
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location =
+            "https://www.linkedin.com/in/anderson-andre-pereira")
+        }
+      >
+        Entrar em contato
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
